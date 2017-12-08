ff2295af387cf2ea3ecdb57b7dc6f0accb84fd2f
Joseph Hellerstein <jlheller@uw.edu>
2017-12-07 19:42:54 -0800

    Updated model

--- coevolutionmodel/Code/state.py
+++ coevolutionmodel/Code/state.py
@@ -7,10 +7,22 @@ State for the dynamical model
 import numpy as np
 import pandas as pd
 
+DEFAULT_MU = 4*10e-9
+DEFAULT_NU = 2*10e-3
+DEFAULT_MU = 1*10e-1
+DEFAULT_NU = 2*10e-1
+DELTA = 0.5
+FITNESS = 0.48
+DEFAULT_FUNC = lambda x,y: FITNESS*(1 - x*(1-y)*DELTA)
 
 class State(object):
 
-  def __init__(mus, nus, fit_func, initial_state=[1, 0]):
+  @classmethod
+  def init(cls,
+      mus = [DEFAULT_MU, DEFAULT_MU], 
+      nus = [DEFAULT_NU, DEFAULT_NU], 
+      fit_funcs = [DEFAULT_FUNC, DEFAULT_FUNC], 
+      initial_state=[1, 0]):
     """
     :param list-of-float mus: beneficial mutation rate
     :param list-of-float nus: deleterious mutation rate
@@ -18,18 +30,16 @@ class State(object):
         that returns a value in [0,1] indicating fitness.
     :param list-of-float initial_state: floats in [0,1]
     """
-    cls = cls.__class__
     cls.mus = mus  # vector of beneficial mutation rates
     cls.nus = nus  # vector of deleterious mutation rates
     cls.state = initial_state
     cls.fit_funcs = fit_funcs
 
   @staticmethod
-  def updateState(state, t):
+  def update(state, t):
     """
     Computes the new value of derivatives of state using 
     the differential equations.
-\dot{x}=(1-x)\nu^{X}+(1-x)F^{X}(x,y)\lambda^{X}-x\mu^{X}-x(1-F^{X}(x,y))\lambda^{X}.
     """
     cls = State
     def computeDerivative(state, index):
@@ -39,9 +49,8 @@ class State(object):
       fit_func = cls.fit_funcs[index]
       fitness = fit_func(state[index], state[1-index])
       dx = (1-x)*nu + (1-x)*fitness - x*mu - x*(1-fitness)
+      return dx
 
-    return [computeDerivative(state, 1), computeDerivative(state, 2)]
-    
-          
-
-  
+    result = [computeDerivative(state, 0), 
+        computeDerivative(state, 1)]
+    return result
